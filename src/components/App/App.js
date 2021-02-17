68b6a8f6e84793b94c5184e3d0f2fd1ee05c6b89
Vada Karlen <vada.karlen@gmail.com>
2021-02-17 16:37:09 -0600

    Guest List is now in its own component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -102,23 +102,7 @@ function App() {
         </div>
         <button type="submit">Add Guest</button>
       </form>
-      <h2>Guest List</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Kid's Meal</th>
-          </tr>
-        </thead>
-        <tbody>
-          {guestList.map((guest) => (
-            <tr key={guest.id}>
-              <td>{guest.name}</td>
-              <td>{String(guest.kidsMeal)}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <GuestList guestList={guestList} />
       <h2>Dinner Supplies</h2>
       <div>Spoons: {guestList.length * 2}</div>
       <div>Forks: {guestList.length * 2}</div>
